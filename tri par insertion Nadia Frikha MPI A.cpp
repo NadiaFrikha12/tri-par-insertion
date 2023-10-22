1ea9442774210260a4427331e952d908e08f97e6
NadiaFrikha12 <148723455+NadiaFrikha12@users.noreply.github.com>
2023-10-22 23:12:56 +0100

    Add files via upload

---
+++ tri par insertion Nadia Frikha MPI A.cpp
@@ -0,0 +1,12 @@
+void TriInsertion (int T[], int taille){
+	for(int i=1 ; i<taille ; i++){
+		int tmp = T[i];
+		int j = i-1 ;
+		while((tmp < T[j]) && (j>=0)){
+			T[j+1]=T[j];
+			j-- ;
+		}
+	T[j+1]=tmp ;
+	}
+}
+
